686302ec8a959d2167daffa644fbc2b3f591b01c
mario-neb <marionebiaj@gmail.com>
2026-08-10 18:40:16 +0200

    fix: only export available templates (2, 4-8) in constants
    Remove templates 1, 3, 9, and 10 from TEMPLATES array since they don't exist in public/templates directory. This fixes the templates not appearing in the selector.

--- src/lib/templates/constants.ts
+++ src/lib/templates/constants.ts
@@ -10,15 +10,12 @@ export const TEMPLATE9 = 9
 export const TEMPLATE10 = 10
 
 export const TEMPLATES = [
-  TEMPLATE1,
   TEMPLATE2,
-  TEMPLATE3,
   TEMPLATE4,
   TEMPLATE5,
   TEMPLATE6,
   TEMPLATE7,
-  TEMPLATE8,
-  TEMPLATE9
+  TEMPLATE8
 ]
 
 export const NEWLINE = '\\\\'
